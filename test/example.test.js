1220728f1d8ebc071c65ce5d0aa6bf71d34ab1b4
Ian Christiansen <ianc.songdog@gmail.com>
2021-10-11 14:48:53 -0700

    working TDD test, but unsure if its the specific test asked for in the lab instructions.

--- test/example.test.js
+++ test/example.test.js
@@ -1,7 +1,7 @@
 // IMPORT MODULES under test here:
 
 import { renderProduct } from "../render-product.js";
-import { product } from "../products.js";
+import { products } from "../products.js";
 
 // import { example } from '../example.js';
 const test = QUnit.test;
@@ -9,12 +9,12 @@ const test = QUnit.test;
 test('renderProduct should return an HTML snippet', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = 'OUTER HTML';
-    const irnBru = sodas[0];
+    const expected = '<div class="product-card"><h2>Necronomicon</h2><img src="./assets/necronomicon.jpg"><p>description of the necronomicon</p><p class="category">supernatural</p><p class="price">$85.00</p><button>add to cart</button></div>';
+    const Necronomicon = products[0];
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = renderProduct(irnBru);
+    const actual = renderProduct(Necronomicon).outerHTML;
 
     //Expect
     // Make assertions about what is expected versus the actual result
