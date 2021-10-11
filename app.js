8943b27f1946a764471e9f27826db6a5a785c88d
Ian Christiansen <ianc.songdog@gmail.com>
2021-10-11 14:12:44 -0700

    submitting working cards with names and image

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// see demo app
+import { renderProduct } from './render-product.js';
+import { products } from './products.js';
+
+
+const productList = document.getElementById('product-list');
+
+for (let item of products){
+  const productCard = renderProduct(item);
+  productList.append(productCard);
+}
--- render-product.js
+++ render-product.js
@@ -1,3 +1,17 @@
-export function renderProduct(productObject){
-    // example from demo
+export function renderProduct(products){
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const productName = document.createElement('h2');
+    productName.textContent = products.name;
+
+    const img = document.createElement('img');
+    img.src = products.image;
+
+    const categoryList = document.createElement('p');
+    categoryList.classList.add('category');
+    categoryList.textContent = products.category;
+
+    productCard.append(productName, img);
+    return productCard;
 }
