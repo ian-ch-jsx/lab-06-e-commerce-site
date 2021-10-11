c9e25612c2d31256e1607717c301eef7074bccc6
Ian Christiansen <ianc.songdog@gmail.com>
2021-10-11 13:47:37 -0700

    committing product array

--- products.js
+++ products.js
@@ -1,14 +1,38 @@
 export const products = [
     {
     id: '1',
-    name: 'granny smith',
-    color: 'green',
-    expDate: Date.now(),
-    type: 'tree fruit',
-    skin: true,
-    season: 'fall',
-    pits: false,
-    weight: '100g',
-    },
-
-]
+    name: 'Necronomicon',
+    image: './assets/necronomicon.jpg',
+    description: 'description of the necronomicon',
+    category: 'supernatural',
+    price: `$85.00`,
+},
+{
+    id: '2',
+    name: 'Eye of Newt',
+    image: './assets/newt-eyes.jpg',
+    description: 'description of eye of newt',
+    category: 'ingredients',
+    price: `$30.00` },
+{
+    id: '3',
+    name: 'Ouija Board',
+    image: './assets/ouija-board.jpg',
+    description: 'pretend this is scary and not just a Hasbro game',
+    category: 'supernatural',
+    price: `$40.00` },
+{
+    id: '4',
+    name: 'Werewolf Drool',
+    image: './assets/werewolf-drool.jpg',
+    description: 'slimy.',
+    category: 'ingredients',
+    price: `$75.00` },
+{
+    id: '5',
+    name: 'Wooden Stake Set',
+    image: './assets/wooden-stake.jpg',
+    description: 'description of the wooden stakes',
+    category: 'tools',
+    price: `$100.00` },
+];
