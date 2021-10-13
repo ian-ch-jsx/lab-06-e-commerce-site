7d640d3317ee8c7c539bf5adb76254b89c44b293
Ian Christiansen <ianc.songdog@gmail.com>
2021-10-13 13:57:26 -0700

    added functioning place order button

--- cart/cart.js
+++ cart/cart.js
@@ -1,9 +1,9 @@
 import { products } from '../data/products.js';
-import { cart } from '../data/cart-data.js';
-import { calculateOrderTotal, findByID, toUSD } from '../utils.js';
+// import { cart } from '../data/cart-data.js';
+import { calculateOrderTotal, findByID, toUSD, getCart } from '../utils.js';
 import { renderLineItem } from '../render-line-items.js';
 
-
+const cart = getCart();
 const tbody = document.getElementById('table-body');
 
 for (let cartItem of cart){
@@ -16,3 +16,9 @@ for (let cartItem of cart){
 const orderTotal = calculateOrderTotal(cart, products);
 const tdOrderTotal = document.getElementById('total');
 tdOrderTotal.textContent = toUSD(orderTotal);
+
+const orderButton = document.getElementById('order-button');
+orderButton.addEventListener('click', ()=>{
+    localStorage.removeItem('CART');
+    window.location.replace('..');
+});
