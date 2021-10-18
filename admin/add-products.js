29a227970dfc4503dea7594531992a767d215c41
Ian Christiansen <ianc.songdog@gmail.com>
2021-10-18 14:13:11 -0700

    submit button creates new listing

---
+++ admin/add-products.js
@@ -0,0 +1,19 @@
+import { addProduct } from '../utils.js';
+
+const form = document.getElementById('add-form');
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const data = new FormData(form);
+    const newProduct = {
+        id: data.get('id'),
+        name: data.get('name-input'),
+        image: data.get('image-input'),
+        description: data.get('description-input'),
+        category: data.get('category-input'),
+        price: Number(data.get('price-input'))
+    };
+
+    addProduct(newProduct);
+    alert('new product listed!');
+});
